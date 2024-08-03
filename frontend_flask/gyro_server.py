c27c7f5d6b80b9948d930375d7b557cd38e42a42
XDMickeyYau <35620428+XDMickeyYau@users.noreply.github.com>
2024-08-03 06:06:57 -0400

    get gyro data from flask frontend server

--- frontend_flask/gyro_server.py
+++ frontend_flask/gyro_server.py
@@ -4,18 +4,26 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+gyroscope_data = []
+
 @socketio.on('connect')
 def handle_connect():
     print('New client connected')
 
 @socketio.on('gyroscopeData')
 def handle_gyroscope_data(data):
+    global gyroscope_data
     print('Gyroscope data received:', data)
-    emit('gyroscopeData', data, broadcast=True)
+    gyroscope_data = data
+    # emit('gyroscopeData', data, broadcast=True)
 
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
+
+def get_gyroscope_data():
+    global gyroscope_data
+    return gyroscope_data
 
 if __name__ == '__main__':
     socketio.run(app, port=4000)
